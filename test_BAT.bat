68d91f71c33c1fdab914aab1f3f23aa2eeb89d8b
Nam Nguyen-Hoai <37695601+hoai97nam@users.noreply.github.com>
2020-06-28 22:34:26 +0700

    auto add
    auto add on Window

---
+++ test_BAT.bat
@@ -0,0 +1,24 @@
+@echo THIS IS TEST CASE FOR ADDING MEMBER BY DOUBLE CLICK ON A BATCH FILE
+@echo PURPOSE: Adding member to Telegram group
+@echo ------------------------------------------------------------------     
+echo           
+                        
+@echo Principle:
+@echo 1. Name of element files must be the same format
+@echo 2. Element files file must be in the same folder
+@echo 3. Consider your resources when multiple adding functions conducted
+echo
+
+REM set /p var1="Enter number of add file: "
+REM set /p var2="Insert py file that you want to use: "
+@echo off
+for /l %%x in (1,1,3) do (
+    start cmd.exe /k python halo%%x.py nam%%x.csv
+)
+REM for /l %%x in (1,1,3) do (
+REM     start cmd.exe /k python halo%%x.py
+REM )
+@echo ON
+
+
+pause >nul
